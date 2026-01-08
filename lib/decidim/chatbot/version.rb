393ecb0dd5866db77601847e10282bf666e91b09
Ivan Vergés <ivan@pokecode.net>
2026-01-08 11:11:33 +0100

    fix spec

--- lib/decidim/chatbot/version.rb
+++ lib/decidim/chatbot/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-chatbot version.
   module Chatbot
     DECIDIM_VERSION = "0.31.0"
-    COMPAT_DECIDIM_VERSION = [">= 0.31.0", "< 0.32"].freeze
+    COMPAT_DECIDIM_VERSION = [">= 0.31.0", "< 0.32.0"].freeze
     VERSION = "0.1.0"
 
     def self.version
