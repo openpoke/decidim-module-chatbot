d45e6ff801d07368e8de09ca8d6d6f5ffb76c784
Ivan Vergés <ivan@pokecode.net>
2026-01-26 20:21:45 +0100

    simpliify

--- app/packs/src/decidim/chatbot/admin/provider_toggle_controller.js
+++ app/packs/src/decidim/chatbot/admin/provider_toggle_controller.js
@@ -3,15 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["checkbox"]
   static values = { url: String }
-  static csrfToken = document.querySelector('meta[name="csrf-token"]') && document.querySelector('meta[name="csrf-token"]').content
-
+  
   toggle() {
     const isEnabled = this.checkboxTarget.checked
+    const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute("content")
 
     fetch(this.urlValue, {
       method: "PATCH",
       headers: {
-        "X-CSRF-Token": this.constructor.csrfToken,
+        "X-CSRF-Token": csrfToken,
         "Accept": "application/json",
         "Content-Type": "application/json"
       }
