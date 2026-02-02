6ed66808a32086f1354d9615cf2caf9f7a35eb68
Anna Topalidi <t.opalidisanna@gmail.com>
2026-02-02 13:51:35 +0100

    improve code

--- app/helpers/decidim/chatbot/application_helper.rb
+++ app/helpers/decidim/chatbot/application_helper.rb
@@ -5,20 +5,20 @@ module Decidim
     module ApplicationHelper
       def participatory_space_options(organization)
         spaces = organization.public_participatory_spaces
-        spaces.group_by { |s| s.class.model_name.human(count: 2) }
-              .transform_values { |arr| arr.map { |s| [translated_attribute(s.title), s.to_gid.to_s] } }
+        spaces.group_by { |space| space.class.model_name.human(count: 2) }
+              .transform_values { |grouped_spaces| grouped_spaces.map { |space| [translated_attribute(space.title), space.to_gid.to_s] } }
       end
 
       def components_for_space(space_gid, organization, component_types: [])
         return [] if space_gid.blank?
 
-        space = organization.public_participatory_spaces.find { |s| s.to_gid.to_s == space_gid }
-        return [] unless space
+        space = GlobalID::Locator.locate(space_gid)
+        return [] unless space&.organization == organization
 
         components = space.components.published
         components = components.where(manifest_name: component_types) if component_types.present?
-        components.map do |c|
-          { id: c.id.to_s, name: translated_attribute(c.name) }
+        components.map do |component|
+          { id: component.id.to_s, name: translated_attribute(component.name) }
         end
       end
 
@@ -26,14 +26,14 @@ module Decidim
         organization.public_participatory_spaces.each_with_object({}) do |space, hash|
           components = space.components.published
           components = components.where(manifest_name: component_types) if component_types.present?
-          hash[space.to_gid.to_s] = components.map { |c| { id: c.id.to_s, name: translated_attribute(c.name) } }
+          hash[space.to_gid.to_s] = components.map { |component| { id: component.id.to_s, name: translated_attribute(component.name) } }
         end.to_json
       end
 
       def delegate_workflow_options(exclude_workflow: nil)
         Decidim::Chatbot.start_workflows_registry.manifests
-                        .reject { |m| m.name.to_s == exclude_workflow.to_s }
-                        .map { |m| [m.title, m.name.to_s] }
+                        .reject { |manifest| manifest.name.to_s == exclude_workflow.to_s }
+                        .map { |manifest| [manifest.title, manifest.name.to_s] }
       end
     end
   end
